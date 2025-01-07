9ce2598615ea7dd578c675f7792311637ab6b344
SamHC23142 <samhc_wang@asus.com>
2025-01-07 18:19:36 +0800

    Homework#1
    Week1-3: Azure OpenAI Introduction
    1.What is Azure OpenAI
    2.Prompt engineering

--- student_assignment.py
+++ student_assignment.py
@@ -4,13 +4,105 @@ import traceback
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+def to_json(response):
+    with open('memorial_day_response.json', 'w', encoding='utf-8') as f:
+        json.dump(response, f, ensure_ascii=False, indent=2)
+    return response
+
 def generate_hw01(question):
-    pass
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+    response_schemas = {
+        "name": "get_memorial_day",
+        "description": "Get information about a Taiwan memorial day",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "Result": {
+                    "type": "object",
+                    "properties": {
+                        "date": {
+                            "type": "string",
+                            "description": "The date of the memorial day in YYYY-MM-DD format",
+                        },
+                        "name": {
+                            "type": "string",
+                            "description": "The name of the memorial day in Traditional Chinese",
+                        },
+                    },
+                    "required": ["date", "name"],
+                },
+            },
+            "required": ["Result"],
+        },
+    }
+
+    structured_llm = llm.with_structured_output(response_schemas)
+
+        
+    examples = [
+        {"input": "2024年1月紀念日", "output":
+        {
+          "Result": 
+          {
+            "date": "2024-01-01",
+            "name": "開國紀念日"
+          }
+        }},
+        {"input": "2024年2月紀念日", "output": 
+        {
+          "Result": 
+          {
+            "date": "2024-02-28",
+            "name": "和平紀念日"
+          }
+        }},
+        {"input": "2024年3月紀念日", "output":
+        {
+          "Result": 
+          {
+            "date": "2024-06-10",
+            "name": "端午節"
+          }
+        }},
+    ]
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt = example_prompt,
+        examples = examples,
+    )
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", """You are an assistant that provides information about Taiwan's memorial days.
+                        please provide the details of most important memorial day in Taiwan that is also a public holiday(in Traditional Chinese)"""),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+        
+    chain = final_prompt | structured_llm
+    
+    response = chain.invoke({"input": question})
+       
+    return to_json(response)
     
 def generate_hw02(question):
     pass
@@ -37,4 +129,4 @@ def demo(question):
     )
     response = llm.invoke([message])
     
-    return response
+    return response   
